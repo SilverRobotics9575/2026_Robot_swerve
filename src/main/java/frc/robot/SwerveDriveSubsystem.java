046c56fdffc365d1a3c8a6c379626d7b14e6963b
Richard McMullin <richard.e.mcmullin@gmail.com>
2026-04-02 08:11:38 -0400

    FIXMEs

--- src/main/java/frc/robot/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/SwerveDriveSubsystem.java
@@ -93,9 +93,6 @@ public class SwerveDriveSubsystem extends SubsystemBase {
         // FIXME speed should be limited to the range +/-1.0. The above
         // equation could end up with a max speed of sqrt(2).
 
-        // TODO idea - is it better to drive backwards when theta is
-        // in the range 90-270 deg? Point in the range +/-90 deg and drive forwards or backwards?
-
         setDriveSpeed(speed);
     }
 
